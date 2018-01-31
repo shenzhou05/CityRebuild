6684efe4cbd40b7714fd9002a3b56d817fb3f3c9
Gu1meter8 <34181454+Gu1meter8@users.noreply.github.com>
2018-01-31 23:14:54 +0800

    readME
    1.仿照https://www.cnblogs.com/qingjoin/p/3630505.html将openImage脚本绑定到mainCinema
    2.创建一个名为game的object（用来测试图片是否选择成功）
    3.图片选择点击open image按钮（全屏左上方），图片可选格式png,jpg,jpng
    4.调用createAcube的函数在openFile脚本的waitLoad函数中（具体参照注释）

---
+++ pathEdit/OpenFile.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System;
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+public class OpenFileName
+{
+    public int structSize = 0;
+    public IntPtr dlgOwner = IntPtr.Zero;
+    public IntPtr instance = IntPtr.Zero;
+    public String filter = null;
+    public String customFilter = null;
+    public int maxCustFilter = 0;
+    public int filterIndex = 0;
+    public String file = null;
+    public int maxFile = 0;
+    public String fileTitle = null;
+    public int maxFileTitle = 0;
+    public String initialDir = null;
+    public String title = null;
+    public int flags = 0;
+    public short fileOffset = 0;
+    public short fileExtension = 0;
+    public String defExt = null;
+    public IntPtr custData = IntPtr.Zero;
+    public IntPtr hook = IntPtr.Zero;
+    public String templateName = null;
+    public IntPtr reservedPtr = IntPtr.Zero;
+    public int reservedInt = 0;
+    public int flagsEx = 0;
+}
+
+public class LocalDialog : MonoBehaviour
+{
+    //链接指定系统函数       打开文件对话框
+    [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+    public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+    public static bool GetOFN([In, Out] OpenFileName ofn)
+    {
+        return GetOpenFileName(ofn);
+    }
+
+    //链接指定系统函数        另存为对话框
+    [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+    public static extern bool GetSaveFileName([In, Out] OpenFileName ofn);
+    public static bool GetSFN([In,Out] OpenFileName ofn)
+    {
+        return GetSaveFileName(ofn); 
+    }
+
+      public static void WaitLoad(string fileName)
+    {
+        try            {                 WWW wwwTexture = new WWW("file://" + fileName);
+                 Texture2D t2d = new Texture2D(200, 200);
+                 wwwTexture.LoadImageIntoTexture(wwwTexture.texture);
+                 GameObject.Find("game").GetComponent<Renderer>().material.mainTexture = wwwTexture.texture;//测试贴图是否生成成功（你测试完之后可以删了）
+                 Debug.Log("创建贴图成功： "+ fileName);                //createACuboid所在类.createACuboid(wwwTexture.texture);//调用你的函数                      }            catch (Exception e)            {               throw e;            }       } 
+    }
+}
+
---
+++ pathEdit/OpenImage.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+using System;
+
+public class OpenImage : MonoBehaviour
+{
+
+    void Start()
+    {
+      
+        OnGUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 50), "Open Image"))
+        {
+            OpenFileName openFileName = new OpenFileName();
+            openFileName.structSize = Marshal.SizeOf(openFileName);
+            openFileName.filter = "图片(*.jpg)\0*.jpg\0图片(*.jpng)\0*.jpng\0图片(*.png)\0*.png";
+            openFileName.file = new string(new char[256]);
+            openFileName.maxFile = openFileName.file.Length;
+            openFileName.fileTitle = new string(new char[64]);
+            openFileName.maxFileTitle = openFileName.fileTitle.Length;
+            openFileName.initialDir = Application.streamingAssetsPath.Replace('/', '\\');//默认路径
+            openFileName.title = "窗口标题";
+            openFileName.flags =
+                0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;
+
+            if (LocalDialog.GetSaveFileName(openFileName))
+            {
+                Debug.Log("获取路径成功："+openFileName.file);
+                Debug.Log("文件名：" + openFileName.fileTitle);
+                LocalDialog.WaitLoad(openFileName.file);
+            }
+        }
+    }
+
+
+}
