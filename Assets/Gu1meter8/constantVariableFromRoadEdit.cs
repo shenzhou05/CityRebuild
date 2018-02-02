f8d44579ec901b7c26482838b44bc1f7251b6bf8
Gu1meter8 <34181454+Gu1meter8@users.noreply.github.com>
2018-02-02 22:27:36 +0800

    俯视+选择图片+点击物体生成小球

---
+++ Assets/Gu1meter8/constantVariableFromRoadEdit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public const string CUBENAME = "game";
+    public const float CAMARAHEIGHT = 10.0f;
+    public const float CUBEHEIGHT = 0.1f;
+    public const string POINTNAME = "point";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Gu1meter8/createPoint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createPoint : MonoBehaviour {
+    static string CUBENAME = "game";
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("鼠标点击");
+                if (hit.collider.gameObject.name == CUBENAME)
+                {
+                    Debug.Log("碰到立方体");
+                    GameObject point = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+                    //(GameObject)Instantiate(Resources.Load("Sphere"));
+                    point.transform.position = new Vector3(hit.point.x, 5, hit.point.z);
+                    point.GetComponent<Renderer>().material.color = Color.red;
+                }
+                    
+                //create a ball
+            }
+
+                
+        }
+    }
+
+}
